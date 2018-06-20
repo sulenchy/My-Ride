50111fb169dfac73d346a49f7073b72362d4ab24
sulenchy <sulaimanabiodun172@gmail.com>
2018-06-20 16:36:57 +0100

    add dummy database

---
+++ server/models/database.js
@@ -0,0 +1,14 @@
+const database = {
+    "rideOffer": {
+        "driver1": [
+            {"leaveTime":"12:00 AM", "destination":"Isolo", "Transport Fare": 1000}            
+        ],
+        "driver2": [
+            {"leaveTime":"3:00 AM", "destination":"Lagos highland", "Transport Fare": 1000}
+        ]
+    }
+}
+
+
+
+export default database;
