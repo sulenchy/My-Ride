e6e43a26b7f92f8b9e8f0a2df88bbf7c7f64c36f
sulenchy <sulaimanabiodun172@gmail.com>
2018-06-23 19:59:37 +0100

    restructure the  dummy  database

--- server/models/database.js
+++ server/models/database.js
@@ -1,12 +1,13 @@
 
 const database = {
-	rideOffer: {
-		ride1: [{ leaveTime: '12:00 AM', destination: 'Isolo', transportFare: 1000 }],
-		ride2: [{ leaveTime: '3:00 AM', destination: 'Lagos highland', transportFare: 1000 }]
-	},
-	request:{
-		
-	}
+	rideOffer: [
+		{ rideId:1, email:"driver1@gmail.com", leaveTime: '12:00 AM', destination: 'Isolo', transportFare: 1000, status: 'Available' },
+		{ rideId:2, email:"driver2@gmail.com", leaveTime: '3:00 AM', destination: 'Lagos highland', transportFare: 1000, status: 'Available'}
+	],
+	request:[
+		{id:1,rideid:1, email:"passenger1@gmail.com",status:"placed"},
+		{id:2,rideId:2, email:"passenger2@gmail.com", status:"placed"}
+	]
 };
 
 export default database;
