0357f8bddff7c10958ac329e481778cf5861816f
sulenchy <sulaimanabiodun172@gmail.com>
2018-07-04 14:21:09 +0100

    bg-fix-test:Fix the bug with the test

--- server/models/database.js
+++ server/models/database.js
@@ -1,9 +1,10 @@
 
 const database = {
-	rideOffer: [
-		{ rideId:1, email:"driver1@gmail.com", leaveTime: '12:00 AM', destination: 'Isolo', transportFare: 1000, status: 'Available' },
-		{ rideId:2, email:"driver2@gmail.com", leaveTime: '3:00 AM', destination: 'Lagos highland', transportFare: 1000, status: 'Available'}
-	],
+	rideOffer: [],
+	// rideOffer: [
+	// 	// { rideId:1, email:"driver1@gmail.com", leaveTime: '12:00 AM', destination: 'Isolo', transportFare: 1000, status: 'Available' },
+	// 	// { rideId:2, email:"driver2@gmail.com", leaveTime: '3:00 AM', destination: 'Lagos highland', transportFare: 1000, status: 'Available'}
+	// ],
 	request:[
 		{id:1,rideid:1, email:"passenger1@gmail.com",status:"placed"},
 		{id:2,rideId:2, email:"passenger2@gmail.com", status:"placed"}
